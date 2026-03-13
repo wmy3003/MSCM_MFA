6752ec7510bfd3586d34c03fbd40a7a8ca8af966
wmy3003 <90093167+wmy3003@users.noreply.github.com>
2026-03-13 19:45:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -19,66 +19,14 @@ img_size2 = 224
 print_frequency = 1
 save_frequency = 5000
 vis_frequency = 5000
+learning_rate = 1e-3
+batch_size = 4
+early_stopping_patience = 100
+print_frequency = 5
+test_session = "Test_session_10.02_16h52"
 
-task_name = 'GlaS'
-# task_name = 'ISIC18'
-# task_name = 'Synapse'
-
-if task_name is 'GlaS':
-    learning_rate = 1e-3
-    batch_size = 4
-    early_stopping_patience = 100
-    print_frequency = 5
-elif task_name is 'ISIC18':
-    learning_rate = 1e-4
-    batch_size = 24
-    early_stopping_patience = 100
-    print_frequency = 30
-elif task_name is "Synapse":
-    learning_rate = 1e-3
-    early_stopping_patience = 100
-    batch_size = 24
-    n_labels = 9
-    n_channels = 1
-    print_frequency = 16
-
-
-# model_name = 'UNet'
-# model_name = 'R34_UNet'
-model_name = 'ULite'
-
-
-if task_name is "ISIC18":
-    if model_name is "UNet":
-        test_session = "Test_session_"
-    if model_name is "R34_UNet":
-        test_session = "Test_session_"
-    if model_name is "ULite":
-        test_session = "Test_session_08.09_03h54"
-
-elif task_name is "GlaS":
-    if model_name is "UNet":
-        test_session = "Test_session_"
-    if model_name is "R34_UNet":
-        test_session = "Test_session_"
-    if model_name is "ULite":
-        test_session = "Test_session_10.02_16h52"
-
-if task_name is "Synapse":
-    if model_name is "UNet":
-        test_session = "Test_session_"
-    if model_name is "R34_UNet":
-        test_session = "Test_session_"
-    if model_name is "ULite":
-        test_session = "Test_session_03.20_17h02"
-
-
-if task_name == 'Synapse':
-    train_dataset = './datasets/Synapse/train_npz/'
-    test_dataset = './datasets/Synapse/test_vol_h5/'
-else:
-    train_dataset = './datasets/'+ task_name+ '/Train_Folder/'
-    test_dataset = './datasets/'+ task_name+ '/Test_Folder/'
+train_dataset = './datasets/'+ task_name+ '/Train_Folder/'
+test_dataset = './datasets/'+ task_name+ '/Test_Folder/'
 
 session_name       = 'Test_session' + '_' + time.strftime('%m.%d_%Hh%M')
 save_path          = task_name +'_kfold/'+ model_name +'/' + session_name + '/'
